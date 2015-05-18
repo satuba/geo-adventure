87479c84a2b9f11a76e54f7d6fc4cb6427512f66
Satu Bailey <satu.bailey@gmail.com>
2015-05-18 15:20:34 -0700

    add challenge routes and models

---
+++ models/Challenge.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var challengeSchema = mongoose.Schema({
+  challengeName: { type: String, required: true},
+  challengeId: String,
+  location: {
+  	latitude: Number,
+  	longitude: Number,
+  	altitude: Number,
+  	timestamp: String
+  },
+  creator: String,
+  image: String,
+  submissionsMsg: String, // only for submitting a challenge, not for creation.
+  submitters: Array,
+  submissionsCount: Number
+});
+
+module.exports = mongoose.model('Challenge', challengeSchema);
