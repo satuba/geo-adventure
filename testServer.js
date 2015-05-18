d7997c47d934b9989271100382644dce2b532ec6
Michael Bowen <mikee.bowen@gmail.com>
2015-05-18 14:44:03 -0700

    start making readfile and writefile functions

---
+++ testServer.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// var mongoose = require('mongoose');
+// var express = require('express');
+// var passport = require('passport');
+// var app = express();
+// var
+
+// app.listen(process.env.PORT || 3000, function() {
+//   console.log('server running on port: ' + (process.env.PORT || 3000));
+// });
+
+
+// request.get({url: 'https://someurl/somefile.torrent', encoding: 'binary'}, function (err, response, body) {
+//   fs.writeFile("/tmp/test.torrent", body, 'binary', function(err) {
+//     if(err)
+//       console.log(err);
+//     else
+//       console.log("The file was saved!");
+//   });
+// });
+
+var write = require('./lib/imageWriter.js');
+
+console.log(write);
+write('hello world', function () {
+  console.log('file saved');
+})
