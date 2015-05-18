5724c17d5b36c97ec596d4a3ce9260d64b4f3fb2
Satu Bailey <satu.bailey@gmail.com>
2015-05-18 15:23:33 -0700

    clean up

--- routes/challenge_routes.js
+++ routes/challenge_routes.js
@@ -5,21 +5,21 @@ var bodyparser = require('body-parser');
 var uuid = require('node-uuid');
 
 module.exports = function(router) {
-	router.use(bodyparser.json());
+  router.use(bodyparser.json());
 
-	//GET request to get all challenges
-	router.get('/challenges', function(req, res) {
-		Challenge.find({}, function(err, data) {
-			if (err) {
-				console.log(err);
-				return res.status(500).json({msg: 'internal server error'});
-			}
-			res.json(data);
-		});
-	});
+  //GET request to get all challenges
+  router.get('/challenges', function(req, res) {
+    Challenge.find({}, function(err, data) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({msg: 'internal server error'});
+      }
+      res.json(data);
+    });
+  });
 
-	//GET request to get a challenge by challenge id
-	router.get('/challenges/:challengeId', function(req, res) {
+  //GET request to get a challenge by challenge id
+  router.get('/challenges/:challengeId', function(req, res) {
     Challenge.findOne({'challengeId': req.params.challengeId}, function(err, data) {
       if(err) {
         console.log(err);
@@ -29,57 +29,52 @@ module.exports = function(router) {
     });
   });
 
-	//POST request creates a new challenge
-	router.post('/challenges/newchallenge', function(req, res) {
-		var randomChallengeId = uuid.v1();
-		var newChallengeData = JSON.parse(JSON.stringify(req.body));
-		var newChallenge = new Challenge(newChallengeData);
-		newChallenge.challengeName = req.body.challengeName;
-		newChallenge.challengeId = randomChallengeId;
-		newChallenge.location.latitude = req.body.latitude;
-		newChallenge.location.longitude = req.body.longitude;
-		newChallenge.location.altitude = req.body.altitude;
-		newChallenge.creator = req.body.creator;
-		newChallenge.image = req.body.image;
-		newChallenge.timestamp = req.body.timestamp;
-		newChallenge.submissionsCount = 0;
-		console.log('challengeId for this challenge: ' + newChallenge.challengeId);
-		newChallenge.save(function(err, data) {
-			if (err) {
-				console.log(err);
-				return res.status(500).json({msg:'internal server error'});
-			}
-			res.json(data);
-		});
-	});
+  //POST request creates a new challenge
+  router.post('/challenges/newchallenge', function(req, res) {
+    var randomChallengeId = uuid.v1();
+    var newChallengeData = JSON.parse(JSON.stringify(req.body));
+    var newChallenge = new Challenge(newChallengeData);
+    newChallenge.challengeName = req.body.challengeName;
+    newChallenge.challengeId = randomChallengeId;
+    newChallenge.location.latitude = req.body.latitude;
+    newChallenge.location.longitude = req.body.longitude;
+    newChallenge.location.altitude = req.body.altitude;
+    newChallenge.creator = req.body.creator;
+    newChallenge.image = req.body.image;
+    newChallenge.timestamp = req.body.timestamp;
+    newChallenge.submissionsCount = 0;
+    console.log('challengeId for this challenge: ' + newChallenge.challengeId);
+    newChallenge.save(function(err, data) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({msg:'internal server error'});
+      }
+      res.json(data);
+    });
+  });
 
-	// PATCH request to submit completed challenge
-	router.patch('/challenges/submit/:challengeId', function(req, res) {
+  // PATCH request to submit completed challenge
+  router.patch('/challenges/submit/:challengeId', function(req, res) {
     Challenge.findOne({'challengeId': req.params.challengeId}, function(err, challenge) {
       if(err) {
         console.log(err);
         return res.status(500).json({msg: 'internal server error'});
       }
-    	challenge.submissionsMsg = req.body.submissionsMsg;
-    	challenge.submissionsCount = challenge.submissionsCount + 1;
+      challenge.submissionsMsg = req.body.submissionsMsg;
+      challenge.submissionsCount = challenge.submissionsCount + 1;
 
-    	// CHANGE CREATOR INTO THE USER THAT SUBMITS THE CHALLENGE!!
-    	(challenge.submitters).push(challenge.creator); 
+      // CHANGE CREATOR INTO THE USER THAT SUBMITS THE CHALLENGE!!
+      (challenge.submitters).push(challenge.creator); 
       
       res.json(challenge);
       challenge.save(function(err) {
-			if (err) {
-				console.log(err);
-				return res.status(500).json({msg:'internal server error'});
-			}
-			console.log('challenge ' + challenge.challengeName + ' completed!');
-			});
+      if (err) {
+        console.log(err);
+        return res.status(500).json({msg:'internal server error'});
+      }
+      console.log('challenge ' + challenge.challengeName + ' completed!');
+      });
     });
-	});
+  });
 
 };
-
-
-
-
-
