0826a59a0a2eed65ade3963c6c1a3522cebd9916
Satu Bailey <satu.bailey@gmail.com>
2015-05-18 10:11:32 -0700

    add stuff to server

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 "use strict";
 
+var mongoose = require("mongoose");
 var express = require("express");
+var passport = require("passport");
 var app = express();
+
+process.env.APP_SECRET = process.env.APP_SECRET || "app wide secret, Claudia";
+
+var challengeRoutes = express.Router();
+var userRoutes = express.Router();
+
+mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/challenges_development");
+
+app.use(passport.initialize());
+
+require("./lib/passport_strategies")(passport);
+
+require("./routes/challenge_routes")(challengeRoutes);
+require(".routes/auth_routes")(userRoutes, passport);
+
+app.use("/api", challenge_routes);
+app.use("/api", userRoutes);
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("server running on port: " + (process.env.PORT || 3000));
