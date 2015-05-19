e092c6200be6c62822e9653d11bacb7d38843a33
Claudia Cuevas <omordn@gmail.com>
2015-05-19 13:00:56 -0700

    troubleshoot port issue

--- server.js
+++ server.js
@@ -34,4 +34,5 @@ app.use('/api', userRoutes);
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('server running on port: ' + (process.env.PORT || 3000));
+  console.log(process.env.PORT);
 });
