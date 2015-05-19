f08e3f87c55b8095845e3ae563e3746cf8eda6b2
Michael Bowen <mikee.bowen@gmail.com>
2015-05-18 18:38:31 -0700

    create working module to create image from base 64 string

--- routes/dummy_routes.js
+++ routes/dummy_routes.js
@@ -1,9 +1,10 @@
 'use strict';
  var bodyparser = require('body-parser');
  var Challenge = require('../models/Challenge');
+ var fs = require('fs');
 
  function createFile (file, callback) {
-  fs.writeFile('../img/test-image-post.jpg', file, function (err, inputFile) {
+  fs.writeFile('./img/test-image-post.jpg', file, function (err, inputFile) {
     if (err) {
       console.log(err);
     }
@@ -13,13 +14,24 @@
   })
 }
 
+function escapeSlashes (string) {
+  return (string + '').replace(/[.?*+^$[\]\\(){}|-]/g, "\\$&");
+}
+
  module.exports = function (router) {
   router.use(bodyparser.json());
 
-  router.post('/dummy/addphoto:image', function (req, res) {
-    var imageBuffer = new Buffer(req.params.image, 'base64');
+  router.post('/dummy/addphoto', function (req, res) {
+    // var originalBuffer = req.body.image;
+    // console.log(originalBuffer);
+    var originalBuffer = escapeSlashes(req.body.image);
+    var imageBuffer = new Buffer(originalBuffer, 'base64');
     createFile(imageBuffer, function () {
+      // if (err) {
+      //   console.log(err);
+      // }
       console.log('file saved');
     })
+    res.json({msg: 'image saved'});
   })
  }
