1c15b1c9c805fb46bfc32551fa9dd11574ce92b6
Claudia Cuevas <omordn@gmail.com>
2015-05-19 08:55:04 -0700

    add working tests, ready for heroku

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-"use strict";
+'use strict';
 
-var mongoose = require("mongoose");
-var express = require("express");
-var passport = require("passport");
+var mongoose = require('mongoose');
+var express = require('express');
+var passport = require('passport');
 var app = express();
 
-process.env.APP_SECRET = process.env.APP_SECRET || "changethisdamnpassword";
+process.env.APP_SECRET = process.env.APP_SECRET || 'changethisdamnpassword';
 
 var challengeRoutes = express.Router();
 var userRoutes = express.Router();
 
-mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/challenges_development");
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/challenges_development');
 
 app.use(passport.initialize());
 
-require("./lib/passport_strat")(passport);
+require('./lib/passport_strat')(passport);
 
-require("./routes/challenge_routes")(challengeRoutes);
-require("./routes/auth_routes")(userRoutes, passport);
+require('./routes/challenge_routes')(challengeRoutes);
+require('./routes/auth_routes')(userRoutes, passport);
 
-app.use("/api", challengeRoutes);
-app.use("/api", userRoutes);
+app.use('/api', challengeRoutes);
+app.use('/api', userRoutes);
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log("server running on port: " + (process.env.PORT || 3000));
+  console.log('server running on port: ' + (process.env.PORT || 3000));
 });
