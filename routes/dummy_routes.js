77e6eb073bc7d615fc3a1700d92459c0e77cc048
Michael Bowen <mikee.bowen@gmail.com>
2015-05-19 10:04:15 -0700

    add comments to createFile function and fix conflict with .gitignore

--- routes/dummy_routes.js
+++ routes/dummy_routes.js
@@ -3,35 +3,40 @@
  var Challenge = require('../models/Challenge');
  var fs = require('fs');
 
- function createFile (file, callback) {
-  fs.writeFile('./img/test-image-post.jpg', file, function (err, inputFile) {
+function createFile (file, callback) {
+  // read the files in the ./img folder and pass it into fs.readdir as list
+  fs.readdir('./img/', function (err, list) {
     if (err) {
       console.log(err);
+      return;
     }
-    if (typeof callback === 'function') {
-      callback();
-    }
-  })
-}
+    // assign the length of the list array to count
+    var count = list.length;
+    // write file to file with counter in the name
+    fs.writeFile('./img/test-image-post' + (count + 1) + '.jpg', file, function (err, inputFile) {
+      if (err) {
+        console.log(err);
+      }
+      // run the callback from the top function
+      if (typeof callback === 'function') {
+        callback();
+      }
+    });
 
-function escapeSlashes (string) {
-  return (string + '').replace(/[.?*+^$[\]\\(){}|-]/g, "\\$&");
+  });
 }
 
  module.exports = function (router) {
   router.use(bodyparser.json());
-
+  // assign route to /dummy/addphoto
   router.post('/dummy/addphoto', function (req, res) {
-    // var originalBuffer = req.body.image;
-    // console.log(originalBuffer);
-    var originalBuffer = escapeSlashes(req.body.image);
-    var imageBuffer = new Buffer(originalBuffer, 'base64');
+    // create a base 64 buffer out of the value of req.body, which should be a base64 string
+    var imageBuffer = new Buffer(req.body.image, 'base64');
+    // run createFile function on the imageBuffer
     createFile(imageBuffer, function () {
-      // if (err) {
-      //   console.log(err);
-      // }
       console.log('file saved');
     })
+    // respond that image has been saved
     res.json({msg: 'image saved'});
   })
  }
