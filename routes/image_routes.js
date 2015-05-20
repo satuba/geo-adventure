3de35bc418d27fb307f19722566e879a0f11b79f
Claudia Cuevas <omordn@Claudias-iMac.local>
2015-05-20 00:06:33 -0700

    rever changes

--- routes/image_routes.js
+++ routes/image_routes.js
@@ -12,7 +12,7 @@ module.exports = function (router) {
     fs.readFile('./img/' + req.params.image, function (err, data) {
       console.log(req.params.image);
       console.log('********DATA********* ' + data);
-      res.status(200).send(data);
+      res.status(200).write(data);
       //res.end();
     });
   });
