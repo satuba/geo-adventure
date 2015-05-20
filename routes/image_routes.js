201c6b7981e30a5bf870ebd779a38270cfeb696b
Claudia Cuevas <omordn@Claudias-iMac.local>
2015-05-19 22:56:18 -0700

    add console log

--- routes/image_routes.js
+++ routes/image_routes.js
@@ -9,7 +9,7 @@ module.exports = function (router) {
   router.use(bodyparser.json());
 
   router.get('/image/:image', function (req, res) {
-    fs.readFile('./img/' + req.params.image, function (err, data) {
+    fs.readFile('./img/' + req.params.image + '.jpg', function (err, data) {
       console.log(data);
       res.write(data);
       res.end();
