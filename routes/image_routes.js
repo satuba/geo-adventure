8fccbf04e1d04032ccddec5958fe16954aafbf88
Michael Bowen <mikee.bowen@gmail.com>
2015-05-21 21:38:27 -0700

    fix liniting fuck

--- routes/image_routes.js
+++ routes/image_routes.js
@@ -18,7 +18,7 @@ module.exports = function(router) {
     });
   });
 
-  router.get('/about', functiong(req, res) {
+  router.get('/about', function(req, res) {
     fs.readFile('./public/index.html', function (err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});
       if (err) {
